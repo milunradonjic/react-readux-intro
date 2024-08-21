7d54bd46accc7d43471b0cd266dee2712a45eb46
milunradonjic <milun.radonjic95@gmail.com>
2024-08-21 14:38:56 +0200

    init commit

---
+++ src/features/accounts/accountSlice.js
@@ -0,0 +1,59 @@
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+export default function accountReducer(state = initialState, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+      };
+    case "account/withdraw":
+      return {
+        ...state,
+        balance: state.balance - action.payload,
+      };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        balance: state.balance + action.payload.loan,
+        loan: action.payload.loan,
+        loanPurpose: action.payload.loanPurpose,
+      };
+    case "account/payLoad":
+      return {
+        ...state,
+        balance: state.balance - state.loan,
+        loan: 0,
+        loanPurpose: "",
+      };
+    default:
+      return state;
+  }
+}
+
+export function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+
+export function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+export function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: {
+      loan: amount,
+      loanPurpose: purpose,
+    },
+  }
+}
+
+export function payLoad() {
+  return { type: "account/payLoad" };
+}
